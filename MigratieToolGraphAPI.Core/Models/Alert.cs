b0aaa7279a323c7f35f76b2b7e0d718e211270b6
Ahmad Alhaj Asaad <ahmadalhajasaad1982@gmail.com>
2026-04-20 06:47:03 +0000

    Voeg Alert-model toe voor het beheren van tijdelijke meldingen

---
+++ MigratieToolGraphAPI.Core/Models/Alert.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MigratieToolGraphAPI.Models
+{
+    public class Alert
+    {
+        public const string AlertKey = "TempDataAlerts";
+        public string Message { get; set; }
+        public string Debug { get; set; }
+    }
+}
